a8f78e134b57e9c20e5a37cecae2f56b9e0043f5
James Summers <jamesnsummers@gmail.com>
2017-05-19 15:29:44 -0500

    added director images and seed info for films and directors

--- seed.js
+++ seed.js
@@ -3,19 +3,56 @@
 
 var db = require('./models');
 
-var new_film =
+var new_film = [
   {
-  title: "Seven",
-  genre: "Thriller",
-  releaseDate: "September 22, 1995",
-  topBilledCast: ["Brad Pitt", "Morgan Freeman", "Kevin Spacey"]
-  }
+    title: "Seven",
+    director: "David Fincher",
+    genre: "Thriller",
+    releaseDate: "September 22, 1995",
+    topBilledCast: ["Brad Pitt", "Morgan Freeman", "Kevin Spacey"]
+  },
+  {
+    title: "The Game",
+    director: "David Fincher",
+    genre: "Thriller",
+    releaseDate: "September 12, 1997",
+    topBilledCast: ["Michael Douglas", "Sean Penn"]
+  },
+  {
+    title: "Short Term 12",
+    director: "Destin Daniel Cretton",
+    genre: "Drama",
+    releaseDate: "August 23, 2013",
+    topBilledCast: ["Brie Larson", "John Gallagher Jr.", "Keith Stanfield"]
+  },
+  {
+    title: "Get Out",
+    director: "Jordan Peele",
+    genre: "Horror",
+    releaseDate: "February 24, 2017",
+    topBilledCast: ["Daniel Kaluuya", "Allison Williams", "Catherine Keener", "Bradley Whitford"]
+  },
 
+];
 
-var director = {
-  name: "David Fincher",
-  alive: true
-}
+var director = [
+  {
+    name: "David Fincher",
+    alive: true,
+    image: "/public/images/David-Fincher.jpg"
+  },
+  {
+    name: "Destin Daniel Cretton",
+    alive: true,
+    image: "/public/images/Destin-Daniel-Cretton.jpg"
+  },
+  {
+    name: "Jordan Peele",
+    alive: true,
+    image: "/public/images/Jordan-Peele.jpg"
+  },
+
+];
 
 db.Films.remove(function(err,succ){
   db.Director.remove(function(err,succ){
